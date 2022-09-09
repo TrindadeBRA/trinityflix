065fcc7a7703221e70b732873ca52be93c621314
Lucas Trindade <trindadebra@gmail.com>
2022-09-08 21:32:05 -0300

    desenvolvimento de outra maneira do api

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,135 +1,151 @@
 async function getData(){
-    const movieID = generateRandomID();
     try {
-        const promiseMoviesDetail = await axios.get(generateRandomMovieUrl(movieID));
-        const promiseMoviesProviders = await axios.get(generateUrlProviders(movieID));
+        const promiseDiscoverMovie = await axios.get(generateDiscoverURL());
+        const discoverMovieData = promiseDiscoverMovie.data;
 
-        const moviesDetail = promiseMoviesDetail.data;
-        const moviesProviders = promiseMoviesProviders.data;
-        
-        console.log(moviesDetail, moviesProviders.results);
-
-        saveMovieDetails(moviesDetail, moviesProviders)
+        separateValues(discoverMovieData)
     } catch (e) {
         getData()
     }
 }
 getData()
 
+async function insertContent(selectedMovie){
+    const movieTitle = selectedMovie.title;
+    const movieResume = selectedMovie.overview;
+    const movieReleaseDate = selectedMovie.release_date.split("-").reverse().join("/");
+    const movieCover = `https://image.tmdb.org/t/p/original${selectedMovie.poster_path}`;
+    const movieGenerosArray = selectedMovie.genre_ids;
 
-function generateRandomID(){
-    const movieID = Math.floor(Math.random() * (10000 - 0) + 0);
-    
-    // const movieID = 9955;
-    console.log("ID SORTEADO: ", movieID);
-    return movieID;
-}
-
-function generateRandomMovieUrl(movieID){
-    const apiKeyURL = "?api_key=454ff844d6893b9ebd5a0f6c518568eb";
-    const languageURL = "&language=pt-BR";
-    const randomMovieUrl = `https://api.themoviedb.org/3/movie/${movieID}${apiKeyURL}${languageURL}&append_to_response=videos`;  
-    return randomMovieUrl
-}
-
-function generateUrlProviders(movieID){
-    const apiKeyURL = "?api_key=454ff844d6893b9ebd5a0f6c518568eb";
-    const urlProvider = `https://api.themoviedb.org/3/movie/${movieID}/watch/providers${apiKeyURL}`;    
-    return urlProvider
-}
-
-function saveMovieDetails(moviesDetail, moviesProviders){
-    const movieTitle = moviesDetail.title;
-    const movieResume = moviesDetail.overview;
-    const movieReleaseDate = moviesDetail.release_date.split("-").reverse().join("/");
-    const movieGenerosArray = moviesDetail.genres;
-    // const movieAdults = moviesDetail.adult;
-    const movieCover = `https://image.tmdb.org/t/p/original${moviesDetail.poster_path}`;
-    
-    if (!('BR' in moviesProviders.results)){
-        throw Error();
-    }
-    
-    if ('flatrate' in moviesProviders.results['BR'] && 'rent' in moviesProviders.results['BR'] && 'buy' in moviesProviders.results['BR']){
-        const movieProvidersFlatrate = moviesProviders.results.BR.flatrate;
-        const movieProvidersRent = moviesProviders.results.BR.rent;
-        const movieProvidersBuy = moviesProviders.results.BR.buy;
-
-        insertContent(movieTitle, movieResume, movieCover, movieReleaseDate, movieGenerosArray, movieProvidersFlatrate, movieProvidersBuy, movieProvidersRent)    
-    } else {
-        throw Error();
-    }
-
-    // if ('rent' in moviesProviders.results['BR']){
-    //     const movieProvidersRent = moviesProviders.results.BR.rent;
-    //     return movieProvidersRent;
-    // }
-
-    // if ('buy' in moviesProviders.results['BR']){
-    //     const movieProvidersBuy = moviesProviders.results.BR.buy;
-    //     return movieProvidersBuy
-    // }
-
-    // if (!('flatrate' in moviesProviders.results['BR'])){
-    //     throw Error();
-    // }
-    
-
-}
-
-async function insertContent(movieTitle, movieResume, movieCover, movieReleaseDate, movieGenerosArray, movieProvidersFlatrate, movieProvidersBuy, movieProvidersRent){
     //Details
     await $(".title-movie").text(movieTitle);
     await $(".resume-movie").text(movieResume); 
     await $(".cover-movie").attr("src",movieCover); 
     await $(".release-date").text(movieReleaseDate); 
     await movieGenerosArray.map(createGenresTags);
-    
-    //Provider
-    await movieProvidersFlatrate.map(createProviderIcons);
-    await movieProvidersBuy.map(createProviderIconsBuy);
-    await movieProvidersRent.map(createProviderIconsRent);
 
-    
+
     setTimeout(() => {
         $(".preloader").removeClass("active");
-    }, 500)
+    }, 750)
+}
+
+function convertGenresNames(genreID){
+    switch (genreID) {
+        case 28:
+            return "Ações";
+            break;
+
+        case 12:
+            return "Aventura";
+            break;
+        
+        case 16:
+            return "Animação";
+            break;
+
+        case 35:
+            return "Comédia";
+            break;
+        
+        case 80:
+            return "Crime";
+            break;
+
+        case 99:
+            return "Documentário";
+            break;
+        
+        case 18:
+            return "Drama";
+            break;
+
+        case 10751:
+            return "Família";
+            break;
+        
+        case 14:
+            return "Fantasia";
+            break;
+
+        case 36:
+            return "História";
+            break;
+        
+        case 27:
+            return "Horror";
+            break;
+
+        case 10402:
+            return "Música";
+            break;
+        
+        case 9648:
+            return "Mistério";
+            break;
+
+        case 10749:
+            return "Romance";
+            break;
+        
+        case 878:
+            return "Ficção Científica";
+            break;
+
+        case 10770:
+            return "TV Movie";
+            break;
+
+        case 53:
+            return "Suspense";
+            break;
+
+        case 10752:
+            return "Guerra";
+            break;
+        
+        case 37:
+            return "Faroeste";
+            break;
+
+        default:
+            break;
+        
+    }
 }
 
 function createGenresTags(movieGenerosArray){
     $(".genres-area").innerHTML = '';
     const movieGenresArea = document.querySelector(".genres-area");
-    var spanTag = document.createElement("span");
-    var genreName = document.createTextNode(movieGenerosArray.name);
-    spanTag.appendChild(genreName);
+    const spanTag = document.createElement("span");
+    const genreName = convertGenresNames(movieGenerosArray)
+    var genreName2 = document.createTextNode(genreName);
+    spanTag.appendChild(genreName2);
     movieGenresArea.appendChild(spanTag);
 }
 
-function createProviderIcons(movieProvidersFlatrate){
-    const movieProviderArea = document.querySelector(".providers-area");
-    $(".providers-area").innerHTML = '';
-    let imageTag = document.createElement("img");
-    const urlImageIcon = `https://www.themoviedb.org/t/p/original${movieProvidersFlatrate.logo_path}`;
-    imageTag.src = urlImageIcon;
-    movieProviderArea.appendChild(imageTag);
+function separateValues(discoverMovieData){
+    const movieResult = getRandomInt(0, 19);
+    const selectedMovie = discoverMovieData.results[movieResult];
+    console.log(selectedMovie)
+
+    insertContent(selectedMovie);
 }
 
-function createProviderIconsBuy(movieProvidersBuy){
-    const movieProviderArea = document.querySelector(".buy-area");
-    $(".buy-area").innerHTML = '';
-    let imageTag = document.createElement("img");
-    const urlImageIcon = `https://www.themoviedb.org/t/p/original${movieProvidersBuy.logo_path}`;
-    imageTag.src = urlImageIcon;
-    movieProviderArea.appendChild(imageTag);
+function generateDiscoverURL(){
+    const paramAPI = "?api_key=454ff844d6893b9ebd5a0f6c518568eb";
+    const paramlanguage = "&language=pt-BR";
+    const page = getRandomInt(1, 100);
+    const paramPage = `&page=${page}`;
+    const paramVoteCount = "&sort_by=vote_count.desc";
+    const discoverURL = `https://api.themoviedb.org/3/discover/movie${paramAPI}${paramlanguage}${paramVoteCount}${paramPage}`;
+    // console.log(discoverURL)
+
+    return discoverURL
 }
 
-function createProviderIconsRent(movieProvidersRent){
-    const movieProviderArea = document.querySelector(".rent-area");
-    $(".rent-area").innerHTML = '';
-    let imageTag = document.createElement("img");
-    const urlImageIcon = `https://www.themoviedb.org/t/p/original${movieProvidersRent.logo_path}`;
-    imageTag.src = urlImageIcon;
-    movieProviderArea.appendChild(imageTag);
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-
