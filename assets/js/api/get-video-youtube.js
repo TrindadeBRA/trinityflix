e373f69f54e5f4014e56c350889a1f04ec40fb24
Lucas Trindade <trindadebra@gmail.com>
2022-09-13 20:32:06 -0300

    Refatoração do CSS

--- assets/js/api/get-video-youtube.js
+++ assets/js/api/get-video-youtube.js
@@ -1,40 +1,40 @@
 
-function searchThisTrailer(movieDetailsData){
-    const movieName = movieDetailsData.title;
-    const queryConvert = movieName.replaceAll(' ', '+');
-    const queryString = `${queryConvert}+trailer+oficial+legendado`;
-    const movieProviderURL = getTrailerYoutubeURL(queryString);
-    const promisseGetTrailerID = getTrailerYT(movieProviderURL);
+// function searchThisTrailer(movieDetailsData){
+//     const movieName = movieDetailsData.title;
+//     const queryConvert = movieName.replaceAll(' ', '+');
+//     const queryString = `${queryConvert}+trailer+oficial+legendado`;
+//     const movieProviderURL = getTrailerYoutubeURL(queryString);
+//     const promisseGetTrailerID = getTrailerYT(movieProviderURL);
 
 
     
-    // const trailerKey = getIdTrailerYT(trailerYTData);
-    // console.log(promisseGetTrailerID);
-    return promisseGetTrailerID;
-}
+//     // const trailerKey = getIdTrailerYT(trailerYTData);
+//     // console.log(promisseGetTrailerID);
+//     return promisseGetTrailerID;
+// }
 
-function getTrailerYoutubeURL(queryString) {
-    const paramAPI = "&key=AIzaSyDuKdfOkczyVa7A384-OOQuh9d5o1wwDa4";
-    const paramQuerySearch = `?q=${queryString}`;
-    const movieProviderURL = `https://www.googleapis.com/youtube/v3/search${paramQuerySearch}${paramAPI}`;
-    return movieProviderURL;
-}
+// function getTrailerYoutubeURL(queryString) {
+//     const paramAPI = "&key=AIzaSyDuKdfOkczyVa7A384-OOQuh9d5o1wwDa4";
+//     const paramQuerySearch = `?q=${queryString}`;
+//     const movieProviderURL = `https://www.googleapis.com/youtube/v3/search${paramQuerySearch}${paramAPI}`;
+//     return movieProviderURL;
+// }
 
-async function getTrailerYT(movieProviderURL){
-    try {
-        const promiseTrailerYT = await axios.get(movieProviderURL);
-        const trailerYTData = promiseTrailerYT.data;
-        const finalKey = getKeyTrailerYT(trailerYTData);
-        return finalKey
-        // getKeyTrailerYT(trailerYTData)
+// async function getTrailerYT(movieProviderURL){
+//     try {
+//         const promiseTrailerYT = await axios.get(movieProviderURL);
+//         const trailerYTData = promiseTrailerYT.data;
+//         const finalKey = getKeyTrailerYT(trailerYTData);
+//         return finalKey
+//         // getKeyTrailerYT(trailerYTData)
 
-    } catch (e) {
-        console.log("ERRO: ",e)
-    }
-}
+//     } catch (e) {
+//         console.log("ERRO: ",e)
+//     }
+// }
 
-function getKeyTrailerYT(trailerYTData){
-    const trailerKey = trailerYTData.items[0].id.videoId;
-    return trailerKey
-}
+// function getKeyTrailerYT(trailerYTData){
+//     const trailerKey = trailerYTData.items[0].id.videoId;
+//     return trailerKey
+// }
 
